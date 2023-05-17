c2186295b7b702e361957f5c4000a23dd0e7c12d
root <josealberto-aguilera>
2023-05-17 07:03:23 +0000

    Versión definitiva de las playbooks

--- recursos/wp-config.php
+++ recursos/wp-config.php
@@ -16,7 +16,7 @@
 define( 'DB_NAME', 'bdwordpress' );
 
 /** Nombre de usuario de MySQL */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', 'wpuser' );
 
 /** Contraseña de MySQL */
 define( 'DB_PASSWORD', '1234' );
@@ -39,3 +39,41 @@ define( 'AUTH_SALT',        'pon_aquí_tu_clave_única' );
 define( 'SECURE_AUTH_SALT', 'pon_aquí_tu_clave_única' );
 define( 'LOGGED_IN_SALT',   'pon_aquí_tu_clave_única' );
 define( 'NONCE_SALT',       'pon_aquí_tu_clave_única' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', true );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
